73b83aa286182b5860bddbdfd86640f2b6557470
Karim Feruzy <167116240+FeruzyNtillah@users.noreply.github.com>
2026-03-11 15:41:59 +0300

    KAMILI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -19,3 +18,4 @@ export default {
 
     plugins: [forms],
 };
+
